deabf09db73de3f65a71c6c84ad95b1dacfb332d
marianasvlt <dhystie@gmail.com>
2025-07-09 11:01:31 -0300

    Add files via upload

---
+++ desafio-concatenacao.js
@@ -0,0 +1,36 @@
+diaDaSemana = prompt('Qual é o dia da semana?');
+
+if (diaDaSemana == 'Quarta-Feira') {
+    alert ('Bom fim de semana!');
+
+} else if (diaDaSemana == 'Quinta-Feira') {
+    alert ('Bom fim de semana!');
+
+} else {
+    alert ('Boa semana!');
+}
+
+numero = prompt ('Digite um positivo ou negativo');
+
+if (numero > 0){
+    alert ('Número positivo!')
+} else { 
+    alert ('Número negativo!')
+}
+
+pontuacao = prompt ('Insira uma pontuação.');
+
+if (pontuacao >= 100) {
+    alert ('Parabéns, você venceu!')
+} else {
+    alert ('Tente novamente para ganhar.')
+}
+
+let saldoConta = 1000; // Exemplo
+alert(`Seu saldo é de R$${saldoConta}.`);
+
+let nome = prompt('Qual o seu nome?');
+alert(`Boas vindas ${nome}`);
+
+
+//Funcionando.
