b56097946b6e8b7cac5211882fe9f2a547be70d5
marianasvlt <dhystie@gmail.com>
2025-07-11 10:59:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-alert ('Boas vindas ao jogo do número secreto'); 
-let numeroSecreto  = 29;
-let chute = prompt ('Escolha um número entre 1 e 30');
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 17;
+console.log(numeroSecreto);
+let chute;
 
-if (chute == numeroSecreto) {
-    console.log ('Isso aí! Você descobriu o número secreto (29)')
+
+//enquanto o chute não for igual ao nS
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 20');
+     //se chute for igual ao nS
+     if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+     } else {
+         if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+         } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+
+    }
+
 }
-console.log('Você errou!')
